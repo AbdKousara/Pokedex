81335a2e1afd4b9e12c64f69510c94f5aef30013
AbdoKousara <abdelkader.kawsara@gmail.com>
2021-06-09 14:18:24 +0200

    minor fix

--- app.js
+++ app.js
@@ -13,19 +13,18 @@ const fetchPokemon = async () =>{
 fetchPokemon();
 
 function createPokemonCard(pokemon){
-    
     let cardElement = document.createElement('div');
     cardElement.classList.add('card');
-    cardElement.innerHTML=`<div class="card_head"><img id="poke_img" src="media/0${pokemon.id}.png" alt="pokemon img"></div>
+    cardElement.innerHTML=`<div class="card_head"><img id="poke_img" 
+         src="media/0${pokemon.id}.png" alt="pokemon img"></div>
          <div class="card_body">
             <span>#00${pokemon.id}</span>
             <h4>${pokemon.name}</h4>
-            ${pokemon.types.length == 2 ? `<p class="pill">${pokemon.types[0].type.name}</p> <p class="pill">${pokemon.types[1].type.name}</p>` 
-            : `<p class="pill">${pokemon.types[0].type.name}</p>`}
+            ${pokemon.types.length == 2 ?
+             `<p class="pill">${pokemon.types[0].type.name}</p> 
+              <p class="pill">${pokemon.types[1].type.name}</p>` 
+            : 
+             `<p class="pill">${pokemon.types[0].type.name}</p>`}
          </div>`;
     document.querySelector('.poke_cards').appendChild(cardElement);
-  
 }
-
-/* ${data[i].type.length == 2 ? `<p class="pill">${data[i].type[0]}</p> <p class="pill">${data[i].type[1]}</p>` 
-            : `<p class="pill">${data[i].type[0]}</p>`} */
